fa24c5d4c7dec4c9f0737ab95ed40890b6c33b3b
Matt <stillwell006@gmail.com>
2023-11-29 17:56:17 -0500

    :sparkles: Add stubs for new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ CommonModule, RouterOutlet, MatSlideToggleModule ],
+  imports: [ CommonModule, RouterOutlet, MatSlideToggleModule, FooterComponent, NavbarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
