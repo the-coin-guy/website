5b1e0d204893db18d34d01ad3c99c7d8b7a53197
Matt <stillwell006@gmail.com>
2023-11-29 16:08:17 -0500

    :art: Add material design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ CommonModule, RouterOutlet, MatSlideToggleModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
