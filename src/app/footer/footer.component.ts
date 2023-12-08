807ed9c37543be55ece6077f89e1a0c94b90ad26
Matt <stillwell006@gmail.com>
2023-12-08 16:03:00 -0500

    :art: improve icon buttons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
     selector: "app-footer",
     standalone: true,
     imports: [
         MatIconModule,
-        FontAwesomeModule
+        FontAwesomeModule,
+        MatButtonModule
     ],
     templateUrl: "./footer.component.html",
     styleUrl: "./footer.component.scss"
@@ -19,4 +22,8 @@ export class FooterComponent {
     @Input() appName!: string;
     currentYear: number = new Date().getUTCFullYear();
     githubIcon: IconDefinition = faGithub;
+    mailIcon: IconDefinition = faEnvelope;
+    githubUrl: string = "https://github.com/the-coin-guy/website";
+    email: string = "alex@gmail.com";
+    mailHref: string = `mailto:${this.email}`;
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FaIconComponent, FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
-import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import { faCoins, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
     selector: "app-navbar",
@@ -23,4 +23,7 @@ import { faCoins } from "@fortawesome/free-solid-svg-icons";
 export class NavbarComponent {
     @Input() appName!: string;
     appIcon: IconDefinition = faCoins;
+    mailIcon: IconDefinition = faEnvelope;
+    email: string = "alex@gmail.com";
+    mailHref: string = `mailto:${this.email}`;
 }
